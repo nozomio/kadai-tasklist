fdeacc3d7eb57bb116553ca34c1b09bb21ef0fb7
nozomi.ochiai <mwam5193@gmail.com>
2019-05-18 09:07:57 +0000

    validation

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_17_055037) do
+ActiveRecord::Schema.define(version: 2019_05_18_085841) do
 
   create_table "tasks", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "title"
+    t.string "status"
   end
 
 end
