31c148020182380eecebeb981d835075a0ca9bc1
nozomi.ochiai <mwam5193@gmail.com>
2019-05-19 13:25:10 +0000

    tasklist

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,20 @@
 Rails.application.routes.draw do
+ 
   get 'users/index'
   get 'users/show'
   get 'users/new'
   get 'users/create'
- root to: 'tasks#index'
+
+  root to: 'tasks#index'
  
- resources :tasks
- 
- get 'signup', to: 'users#new'
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
+
+  resources :tasks
+
+  get 'signup', to: 'users#new' # UsersControllerのnewへのaliasを貼っている
   resources :users, only: [:index, :show, :new, :create]
+  
+  resources :tasks , only:[:create, :destroy]
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_19_083451) do
+ActiveRecord::Schema.define(version: 2019_05_19_122131) do
 
   create_table "tasks", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "status"
+    t.bigint "users_id"
+    t.index ["users_id"], name: "index_tasks_on_users_id"
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
@@ -27,4 +29,5 @@ ActiveRecord::Schema.define(version: 2019_05_19_083451) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "tasks", "users", column: "users_id"
 end
